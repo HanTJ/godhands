913fcfe9f0e769b4328aad8c241d195392a6444b
HanTJ <41634614+HanTJ@users.noreply.github.com>
2022-11-27 15:37:28 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.py
@@ -0,0 +1,6 @@
+basic.show_icon(IconNames.HEART)
+SuperBit.motor_run(SuperBit.enMotors.M2, 0)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,19 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Heart)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+        SuperBit.MotorRun(SuperBit.enMotors.M2, 100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+        SuperBit.MotorRun(SuperBit.enMotors.M2, -100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+        SuperBit.MotorRun(SuperBit.enMotors.M4, 100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+        SuperBit.MotorRun(SuperBit.enMotors.M4, -100)
+    } else {
+        SuperBit.MotorStopAll()
+    }
+})
