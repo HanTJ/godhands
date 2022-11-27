e6807441e7151e67888d97d6dabbe1bbf6cc5f2a
HanTJ <41634614+HanTJ@users.noreply.github.com>
2022-11-27 18:30:51 +0900

    모터4개 모두 설정완료

--- main.ts
+++ main.ts
@@ -13,6 +13,14 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
         SuperBit.MotorRun(SuperBit.enMotors.M4, 100)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
         SuperBit.MotorRun(SuperBit.enMotors.M4, -100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 100)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, -100)
     } else {
         SuperBit.MotorStopAll()
     }
